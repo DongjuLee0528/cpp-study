0aa156b69e0e53215a623c1a1e2f1e078df3d4f3
DongjuLee <dozoo0528@gmail.com>
2025-12-16 20:15:33 +0900

    함수안에 함수 사용하기

---
+++ src/chapter1/chapter1_5_2.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#include <iostream>
+using namespace std;
+
+void print() {
+    cout <<"Hello"<<endl;
+}
+
+
+int addTwoNumbers(int a, int b) {
+    //return a + b;
+    print();
+}
+int main() {
+    //int sum = addTwoNumbers(1, 2);
+    addTwoNumbers(1, 2);
+
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_5_2.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#ifndef CPPLECTURE_CHAPTER1_5_2_H
+#define CPPLECTURE_CHAPTER1_5_2_H
+
+
+class chapter1_5_2 {
+};
+
+
+#endif //CPPLECTURE_CHAPTER1_5_2_H
