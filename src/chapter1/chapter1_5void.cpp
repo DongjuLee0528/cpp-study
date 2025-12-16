50cb3b0ab7910e79f5dadd40b69822e8ff6c4366
DongjuLee <dozoo0528@gmail.com>
2025-12-16 22:42:27 +0900

    void main 설명

--- src/chapter1/chapter1_5void.cpp
+++ src/chapter1/chapter1_5void.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 using namespace std;
 
-void printHelloWorld() {
+void printHelloWorld() { //void는 반환값이 없는 함수 타임 //main은 os가 호출 하는 프로그램의 시작 함수
     cout <<"Hello world!" << endl;
 }
 
