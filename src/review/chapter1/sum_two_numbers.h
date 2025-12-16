6d9f49275472c0c1f6bc57e7aca6525adf701c9e
DongjuLee <dozoo0528@gmail.com>
2025-12-16 20:16:02 +0900

    혼자 공부 2수 입력 받아 더하기

---
+++ src/review/chapter1/sum_two_numbers.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#ifndef CPPLECTURE_SUM_TWO_NUMBERS_H
+#define CPPLECTURE_SUM_TWO_NUMBERS_H
+
+
+class sum_two_numbers {
+};
+
+
+#endif //CPPLECTURE_SUM_TWO_NUMBERS_H
