df4dba9d60c13e0e32353c22179188c20f9d65e7
DongjuLee <dozoo0528@gmail.com>
2025-12-16 15:33:51 +0900

    수정

---
+++ src/chapter1/chapter1_1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Dongju Lee on 2025. 12. 14..
+//
+
+#include <iostream>
+int main(void) {
+
+    int x= 2;
+    int y= x+3;
+
+    std::cout << y << std::endl;
+    std::cout << 1 + 2 << std::endl;
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_1.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 14..
+//
+
+#ifndef CPP_CHAPTER1_1_H
+#define CPP_CHAPTER1_1_H
+
+
+class chapter1_1 {
+};
+
+
+#endif //CPP_CHAPTER1_1_H
