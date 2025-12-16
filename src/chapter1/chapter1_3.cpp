e3f2d5c5a998e130811b49f49f9f513387b286e9
DongjuLee <dozoo0528@gmail.com>
2025-12-16 15:43:48 +0900

    변수

---
+++ src/chapter1/chapter1_3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#include <iostream>
+
+int main() {
+
+    int x = 123;
+    std :: cout << x << std :: endl;
+    std :: cout << &x << std :: endl;
+
+    return 0;
+}
