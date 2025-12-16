6d4680b92ea50753e9a1c130cafd914892b98105
DongjuLee <dozoo0528@gmail.com>
2025-12-16 19:00:44 +0900

    void

---
+++ src/chapter1/chapter1_5void.cpp
@@ -0,0 +1,18 @@
+
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#include <iostream>
+using namespace std;
+
+void printHelloWorld() {
+    cout <<"Hello world!" << endl;
+}
+
+int main (){
+
+    printHelloWorld();
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_5void.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 16..
+//
+
+#ifndef CPPLECTURE_CHAPTER1_5VOID_H
+#define CPPLECTURE_CHAPTER1_5VOID_H
+
+
+class chapter1_5void {
+};
+
+
+#endif //CPPLECTURE_CHAPTER1_5VOID_H
