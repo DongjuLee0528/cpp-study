277394cfb5c872d710207297db3e7fe3df27d182
DongjuLee <dozoo0528@gmail.com>
2025-12-17 14:16:31 +0900

    키워드와 식별자 이름짓기

---
+++ src/chapter1/chapter1_6.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Dongju Lee on 2025. 12. 17..
+//
+
+#include <iostream>
+
+using namespace std;
+
+void GetNumber() {
+
+}
+
+class MyClass {
+    public:
+    int m_variable;
+};
+
+int main() {
+
+    int num_apples;
+    int value;
+    int nApples;
+    int _apples;
+
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_6.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 17..
+//
+
+#ifndef CPPLECTURE_CHAPTER1_6_H
+#define CPPLECTURE_CHAPTER1_6_H
+
+
+class chapter1_6 {
+};
+
+
+#endif //CPPLECTURE_CHAPTER1_6_H
