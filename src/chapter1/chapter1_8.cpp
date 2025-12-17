05b419f255fff5e4a3e57dfc1507e8d04cf5d8f0
DongjuLee <dozoo0528@gmail.com>
2025-12-17 14:54:48 +0900

    연산자

---
+++ src/chapter1/chapter1_8.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dongju Lee on 2025. 12. 17..
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 2;
+    int y = (x > 0) ? 1:2;
+
+    cout << x + 2<< endl;
+    cout << y << endl;
+
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_8.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 17..
+//
+
+#ifndef CPPLECTURE_CHAPTER1_8_H
+#define CPPLECTURE_CHAPTER1_8_H
+
+
+class chapter1_8 {
+};
+
+
+#endif //CPPLECTURE_CHAPTER1_8_H
