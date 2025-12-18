1f73d442614215b1a6163eb955a233dcf57fcc08
DongjuLee <dozoo0528@gmail.com>
2025-12-18 19:55:24 +0900

    기본적인 서식

---
+++ src/chapter1/chapter1_9.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Dongju Lee on 2025. 12. 18..
+//
+
+#include <iostream>
+
+using namespace std;
+
+int add (int x, int y) {
+    return x+y;
+}
+int main() {
+
+    int my_v       =1;
+    int x          =4;
+    int num_apples = 123;
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_9.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2025. 12. 18..
+//
+
+#ifndef CPPLECTURE_CHAPTER1_9_H
+#define CPPLECTURE_CHAPTER1_9_H
+
+
+class chapter1_9 {
+};
+
+
+#endif //CPPLECTURE_CHAPTER1_9_H
