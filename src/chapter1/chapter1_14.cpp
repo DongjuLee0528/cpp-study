0c707faa9a7799e7d1fb8b6ce73ab8a8a7ee2613
DongjuLee <dozoo0528@gmail.com>
2025-12-20 16:03:41 +0900

    전처리기와의 첫 만남

---
+++ src/chapter1/chapter1_14.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Dongju Lee on 2025. 12. 20..
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define LIKE_APPLE
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+void doSomething() {
+
+}
+
+
+int main() {
+
+
+    doSomething();
+    cout << MAX(1+3,2) << endl;
+    cout << MAX(1+3,2) << endl;
+
+    return 0;
+}
---
+++ src/chapter1/chapter1_14.h
@@ -0,0 +1,14 @@
+//
+// Created by Dongju Lee on 2025. 12. 20..
+//
+#pragma once
+
+void doSomething() {
+#ifdef LIKE_APPLE
+    cout <<"Apple" << endl;
+#endif
+
+#ifdef LIKE_APPLE
+    cout <<"Orage" << endl;
+#endif
+}
