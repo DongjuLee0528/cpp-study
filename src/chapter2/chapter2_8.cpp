b8edc6b140abc5dd6aa75d7174a8b8c2e95028df
DongjuLee <dozoo0528@gmail.com>
2026-01-01 18:53:16 +0900

    리터럴 상수

---
+++ src/chapter2/chapter2_8.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Dongju Lee on 2026. 1. 1..
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+     int x = 012; // 8진수 표현하고 싶을때만 0을 앞에 사용 16진수는 0x 붙임
+     int h = 0xF;
+     int b = 0b1010;
+    cout << x <<endl;
+    cout << h <<endl;
+    cout << b <<endl;
+
+
+    return 0;
+}
---
+++ src/chapter2/chapter2_8.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 1. 1..
+//
+
+#ifndef CPP_STUDY_CHAPTER2_8_H
+#define CPP_STUDY_CHAPTER2_8_H
+
+
+class chapter2_8 {
+};
+
+
+#endif //CPP_STUDY_CHAPTER2_8_H
