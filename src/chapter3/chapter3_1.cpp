7d6f8747c2326c9595aba29490a323196f7d8a27
DongjuLee <dozoo0528@gmail.com>
2026-01-02 11:49:57 +0900

    연산자 우선순위와 결합 법칙

---
+++ src/chapter3/chapter3_1.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Dongju Lee on 2026. 1. 2..
+//
+// https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // 계산 우선 순위는 수학과 같이 계산한다
+    int x = std::pow(2,3); // 제곱 사용
+    cout << x << endl;
+
+
+    return 0;
+}
---
+++ src/chapter3/chapter3_1.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 1. 2..
+//
+
+#ifndef CPP_STUDY_CHAPTER3_1_H
+#define CPP_STUDY_CHAPTER3_1_H
+
+
+class chapter3_1 {
+};
+
+
+#endif //CPP_STUDY_CHAPTER3_1_H
