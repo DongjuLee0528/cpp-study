91b0bbb4008b768b92e8184d2b6adfa5c35a6fdb
DongjuLee <dozoo0528@gmail.com>
2026-01-30 20:38:35 +0900

    산술 연산자

---
+++ src/chapter3/chapter3_2.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Dongju Lee on 2026. 1. 30..
+//
+
+#include <iostream>
+using namespace std;
+int main () {
+
+    int x =7;
+    int y = 4;
+
+    cout << x / y << endl;
+    cout << float(x) / y << endl;
+    cout << x / float(y) << endl;
+
+    int z = x;
+    z +=y; //z = z+y
+
+    return 0;
+}
---
+++ src/chapter3/chapter3_2.h
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 1. 30..
+//
+
+#ifndef CPP_STUDY_CHAPTER3_2_H
+#define CPP_STUDY_CHAPTER3_2_H
+
+
+class chapter3_2 {
+};
+
+
+#endif //CPP_STUDY_CHAPTER3_2_H
